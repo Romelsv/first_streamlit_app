a7b3bcb3f05ca7c239dbcafc44195efb0a952114
romel-as <104181285+Romelsv@users.noreply.github.com>
2022-11-21 11:53:27 +0000

    Update streamlit_app.py
    imported pandas
    connected the s3 bucket to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit 
-
+import pandas
 
 streamlit.title('My parents New healty Diner')
 
@@ -9,4 +9,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado and Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
