38cb52e770d7a8c8e691a07716c155ce130b62c9
romel-as <104181285+Romelsv@users.noreply.github.com>
2022-11-21 11:58:28 +0000

    Update streamlit_app.py
    changing number index to to fruit labels

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ streamlit.text('🥑🍞 Avocado and Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 #Pick alist here, customer can pick the fruit
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
