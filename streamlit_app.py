4fd33b5efe36d863beffbcfd348c4ca0508782ea
romel-as <104181285+Romelsv@users.noreply.github.com>
2022-11-21 12:56:04 +0000

    Update streamlit_app.py
    added filtering for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 #Pick alist here, customer can pick the fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_to_show=my_fruit_list.loc[fruits_selected]
 
 #diplay the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_to_show)
 
