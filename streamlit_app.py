0987f0b195936838611ccaa388466092608a82ee
romel-as <104181285+Romelsv@users.noreply.github.com>
2022-11-22 13:36:32 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# convert json to dict
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put them in a pandas dataframe
+streamlit.dataframe(fruityvice_normalized)
