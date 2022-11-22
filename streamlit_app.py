032a983caf509ef445bbb6a7fdd2e82a20a8daf2
romel-as <104181285+Romelsv@users.noreply.github.com>
2022-11-22 15:44:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put them in a pandas dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+import snowlfake.connector
